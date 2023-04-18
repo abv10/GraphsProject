88afaa0f3043fed64c1b8025641cab0b3f9c52d6
Alex Vaslow <abv10@case.edu>
2023-04-18 16:08:32 -0400

    IOU should be working

--- EvaluationHelpers.py
+++ EvaluationHelpers.py
@@ -32,11 +32,19 @@ def IOU(y_target, y_predict):
     y_target = H * W
     y_predict = H * W * C
     '''
-    y_arg = torch.argmax(y_predict, dim=0)
-    intersection = torch.eq(y_arg, y_target).sum(dim=[0, 1])
-    union = (y_arg != 0).sum(dim=[0,1]) + (y_target != 0).sum(dim=[0,1]) - intersection
+    length = y_predict.size(1)
+    y_arg = torch.argmax(y_predict, dim=1)
+    print(y_arg.shape)
+    print(y_target.shape)
+    print(y_arg)
+    print(y_target)
+    intersection = (torch.eq(y_arg, y_target) & (y_arg != 0)).sum(dim=[1, 2])
+    union = torch.zeros_like(intersection)
+    for l in range(1,length):
+        union = union + (y_arg == l).sum(dim=[1,2])
+        union = union + (y_target == l).sum(dim=[1,2])
+    union = union - intersection
     iou = intersection.float() / union.float()
-    print(intersection, union)
     return iou.mean().item()
 
 def evaluate(dataset, path):
@@ -64,14 +72,16 @@ def evaluate(dataset, path):
 
                 outputs = model(inputs).cpu()
 
-                for d in range(inputs.size(0)):
-                    iou_sum += IOU(y_target=targets[d], y_predict=outputs[d])
-                    iou_count += 1
+                iou_sum += IOU(y_target=targets, y_predict=outputs)
+                iou_count += 1
 
     print(iou_sum/iou_count)
 
 
 if __name__ == "__main__":
+    #target = torch.randint(low=0, high=3, size=(16, 256, 256))
+    #predicted = torch.randn((16,3,256,256))
+    #print(IOU(target, predicted))
     evaluate("prostate","best_prostate.pt")
 
 

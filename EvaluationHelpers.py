dbdcf0b3369c75b2715fb5a341a57189ab99af3d
Vaslow;Alex <abv10@case.edu>
2023-04-23 21:30:42 -0400

    Evaluation Helpers

--- EvaluationHelpers.py
+++ EvaluationHelpers.py
@@ -4,6 +4,7 @@ from ProstateDataset import ProstateDataset
 from LITSDataset import LITSDataset 
 from torch.utils.data import Dataset, DataLoader
 from UNET import UNet
+from TransformerNet import TransUNet
 
 def IOU(y_target, y_predict):
     '''
@@ -30,8 +31,9 @@ def precision(y_target, y_predict):
     length = y_predict.size(1)
     y_arg = torch.argmax(y_predict, dim=1)
     true_positives = ((y_arg == y_target) & (y_target != 0)).sum(dim=[1, 2])
-    false_positives = ((y_arg != y_target) & (y_target != 0)).sum(dim=[1, 2])
-    precision = true_positives.float() / (true_positives + false_positives).float()
+    false_positives = ((y_arg != y_target) & (y_target == 0)).sum(dim=[1, 2])
+    precision = true_positives.float() / torch.max((true_positives + false_positives).float(),torch.ones((16,1)))  
+
     return precision.mean().item()
 
 def recall(y_target, y_predict):
@@ -42,8 +44,9 @@ def recall(y_target, y_predict):
     length = y_predict.size(1)
     y_arg = torch.argmax(y_predict, dim=1)
     true_positives = ((y_arg == y_target) & (y_target != 0)).sum(dim=[1, 2])
-    false_negatives = ((y_arg != y_target) & (y_arg == 0)).sum(dim=[1, 2])
-    recall = true_positives.float() / (true_positives + false_negatives).float()
+    false_negatives = ((y_arg != y_target) & (y_target != 0)).sum(dim=[1, 2])
+    recall = true_positives.float() / torch.max((true_positives + false_negatives).float(),torch.ones((16,1)))
+
     return recall.mean().item()
 
 
@@ -59,17 +62,36 @@ def DICE(y_target, y_predict):
     for l in range(1,length):
         union = union + (y_arg == l).sum(dim=[1,2])
         union = union + (y_target == l).sum(dim=[1,2])
-    dice = 2 * intersection.float() / union.float()
+    dice = 2 * intersection.float() / torch.max(union.float(),torch.ones((16,1)))
     return dice.mean().item()
 
-def evaluate(dataset, path):
+def evaluate(dataset, path, network="UNET"):
     if dataset == "prostate":
-        val_dataset = ProstateDataset(val_fold=0, validation=True)
-        model = UNet(in_channels=3, out_channels=2)
-
+        val_dataset = ProstateDataset(val_fold=0, validation=True, size=(256,256))
+        if network == "UNET":
+            model = UNet(in_channels=3, out_channels=2)
+        else:
+            model = TransUNet(img_dim=128,
+                            in_channels=3,
+                            out_channels=128,
+                            head_num=4,
+                            mlp_dim=512,
+                            block_num=8,
+                            patch_dim=16,
+                            class_num=2)
     else:
-        val_dataset = LITSDataset(val_fold=0, validation=True)
-        model = UNet(in_channels=3, out_channels=3)
+        val_dataset = LITSDataset(val_fold=0, validation=True, size=(128,128))
+        if network == "UNET":
+            model = UNet(in_channels=3, out_channels=3)
+        else:
+            model = TransUNet(img_dim=128,
+                            in_channels=3,
+                            out_channels=128,
+                            head_num=4,
+                            mlp_dim=512,
+                            block_num=8,
+                            patch_dim=16,
+                            class_num=3)
 
 
     val_loader = DataLoader(val_dataset, batch_size=16, shuffle=True)
@@ -81,6 +103,8 @@ def evaluate(dataset, path):
     with torch.no_grad():
             iou_sum = 0
             dice_sum = 0
+            recall_sum = 0
+            precision_sum = 0
             iou_count = 0
             for data, targets in val_loader:
                 inputs = data.to(device)
@@ -90,20 +114,19 @@ def evaluate(dataset, path):
 
                 iou_sum += IOU(y_target=targets, y_predict=outputs)
                 dice_sum += DICE(y_target=targets, y_predict=outputs)
+                recall_sum += recall(y_target=targets, y_predict=outputs)
+                precision_sum += precision(y_target=targets, y_predict=outputs)
                 iou_count += 1
-                print(iou_sum, iou_count)
+                print(iou_sum, dice_sum)
 
     print("DICE:",dice_sum/iou_count)
     print("IOU:",iou_sum/iou_count)
-
+    print("RECALL:",recall_sum/iou_count)
+    print("Precision:",precision_sum/iou_count)
 
 
 if __name__ == "__main__":
     #target = torch.randint(low=0, high=3, size=(16, 256, 256))
     #predicted = torch.randn((16,3,256,256))
     #print(IOU(target, predicted))
-    evaluate("prostate","best_prostate.pt")
-
-
-
-        
+    evaluate("lits","best_trans_lits.pt", network="lits")

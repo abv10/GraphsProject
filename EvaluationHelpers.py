e3a60eac5fb0c2dbc65c1021037431bebd8f873d
Alex Vaslow <abv10@case.edu>
2023-05-08 17:43:35 -0400

    Evaluation Helpers.py

--- EvaluationHelpers.py
+++ EvaluationHelpers.py
@@ -5,6 +5,7 @@ from LITSDataset import LITSDataset
 from torch.utils.data import Dataset, DataLoader
 from UNET import UNet
 from TransformerNet import TransUNet
+from PIL import Image 
 
 def IOU(y_target, y_predict, reduce=False):
     '''
@@ -147,4 +148,65 @@ if __name__ == "__main__":
     #target = torch.randint(low=0, high=3, size=(16, 256, 256))
     #predicted = torch.randn((16,3,256,256))
     #print(IOU(target, predicted))
-    evaluate("lits","best_UNET_lits.pt", network="UNET")
+    #evaluate("lits","best_UNET_lits.pt", network="UNET")
+    dataset = "prostate"
+    network = "UNET"
+    path = ""
+    if dataset == "prostate":
+        if network == "UNET":
+            size = (256,256)
+            model = UNet(in_channels=3, out_channels=2)
+        else:
+            size = (128, 128)
+            model = TransUNet(img_dim=128,
+                            in_channels=3,
+                            out_channels=128,
+                            head_num=4,
+                            mlp_dim=512,
+                            block_num=8,
+                            patch_dim=16,
+                            class_num=2)
+    else:
+        if network == "UNET":
+            size = (256, 256)
+            model = UNet(in_channels=3, out_channels=3)
+        else:
+            size = (128, 128)
+            model = TransUNet(img_dim=128,
+                            in_channels=3,
+                            out_channels=128,
+                            head_num=4,
+                            mlp_dim=512,
+                            block_num=8,
+                            patch_dim=16,
+                            class_num=3)
+    #model.load_state_dict(torch.load(path))
+   
+    model.eval()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+    with torch.no_grad():
+        image_path = "ProstateX/Processed/p0056slice7.png"
+
+        image = Image.open(image_path)
+        image = image.convert("RGB")
+        image = image.resize(size)
+
+        image = np.array(image)
+        image = np.transpose(image, (2, 0, 1))
+        image = np.expand_dims(image, axis=0)
+        image = torch.from_numpy(image).float()
+
+        inputs = image.to(device)
+                
+
+        outputs = model(inputs).cpu()
+        outputs = torch.argmax(outputs, dim=1) * 255
+        outputs = torch.squeeze(outputs).numpy()
+
+        # convert the tensor to a grayscale PIL image
+        pil_image = Image.fromarray(outputs.astype('uint8'), mode='L')
+
+        # save the image
+        pil_image.save('output.jpg')
+        print(outputs)
